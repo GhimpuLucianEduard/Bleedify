872ac9483f8df57b4bf662240d3728313a131d91
EdyKovacs <edy.kovacs3@gmail.com>
2018-05-05 11:36:22 +0300

    bla

---
+++ Bleedify/BleedifyServices/DonatieService.cs
@@ -0,0 +1,41 @@
+﻿using BleedifyModels.ModelsEF;
+using BleedifyModels.Repositories;
+using BleedifyModels.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BleedifyServices
+{
+    public class DonatieService
+    {
+        private IRepository<int, Donatie> _repository;
+
+        public DonatieService()
+        {
+            _repository = new DonatieRepository(new DonatieValidator());
+        }
+
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+
+        public Donatie Find(int id)
+        {
+            return _repository.Find(id);
+        }
+
+        public IEnumerable<Donatie> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public void Update(Donatie entity)
+        {
+            _repository.Update(entity);
+        }
+    }
+}
