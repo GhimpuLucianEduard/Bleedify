007fb1d067f723c83849a422ba86467f9a5a64c4
EdyKovacs <edy.kovacs3@gmail.com>
2018-05-05 12:41:16 +0300

    register Personal

---
+++ Bleedify/BleedifyServices/PersonalService.cs
@@ -0,0 +1,26 @@
+﻿using BleedifyModels.ModelsEF;
+using BleedifyModels.Repositories;
+using BleedifyModels.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BleedifyServices
+{
+    public class PersonalService
+    {
+        private IRepository<int, Personal> _repository;
+
+        public PersonalService()
+        {
+            _repository = new PersonalRepository(new PersonalValidator());
+        }
+        
+        public void Register(Personal personal)
+        {
+            _repository.Add(personal);
+        }
+    }
+}
