961e1acea5ac1817c86542930ab718415be9b886
Ghimpu Lucian Eduard <ghimpulucianeduard@gmail.com>
2018-05-11 21:08:08 +0300

    Base view model.

---
+++ Bleedify/DomainViewModels/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainViewModels
+{
+    public class Class1
+    {
+    }
+}
