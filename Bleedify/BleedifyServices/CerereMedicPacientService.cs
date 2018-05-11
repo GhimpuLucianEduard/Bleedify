b39a7b150f3acb138f8db103c3f82847b34b5db6
AndreiGhiurea <andreis181@gmail.com>
2018-05-11 22:19:46 +0300

    Grupa, medic, pacient, cerere services
    Facui serviceurile

---
+++ Bleedify/BleedifyServices/CerereMedicPacientService.cs
@@ -0,0 +1,42 @@
+﻿using BleedifyModels.ModelsEF;
+using BleedifyModels.Repositories;
+using BleedifyModels.Validators;
+using System.Collections.Generic;
+
+namespace BleedifyServices
+{
+    class CerereMedicPacientService
+    {
+        private IRepository<int, CerereMedicPacient> _repository;
+
+        public CerereMedicPacientService()
+        {
+            _repository = new CerereMedicPacientRepository(new CerereMedicPacientValidator());
+        }
+
+        public void Add(CerereMedicPacient entity)
+        {
+            _repository.Add(entity);
+        }
+
+        public CerereMedicPacient Find(int id)
+        {
+            return _repository.Find(id);
+        }
+
+        public IEnumerable<CerereMedicPacient> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public void Update(CerereMedicPacient entity)
+        {
+            _repository.Update(entity);
+        }
+
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
---
+++ Bleedify/BleedifyServices/GrupaDeSangeService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using BleedifyModels.ModelsEF;
+using BleedifyModels.Repositories;
+using BleedifyModels.Validators;
+
+namespace BleedifyServices
+{
+    class GrupaDeSangeService
+    {
+        private IRepository<int, GrupaDeSange> _repository;
+
+        public GrupaDeSangeService()
+        {
+            _repository = new GrupaDeSangeRepository(new GrupaDeSangeValidator());
+        }
+
+        public IEnumerable<GrupaDeSange> GetAll()
+        {
+            return _repository.GetAll();
+        }
+    }
+}
