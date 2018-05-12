7725c47ca78a7d143ceb3372d749c4418707fce6
Ghimpu Lucian Eduard <ghimpulucianeduard@gmail.com>
2018-05-12 16:13:29 +0300

    Fix

--- Bleedify/BleedifyServices/AppService.cs
+++ Bleedify/BleedifyServices/AppService.cs
@@ -32,10 +32,12 @@ namespace BleedifyServices
             get { return _institutieService; }
         }
 
-        public DonatorService DonatorService
-        {
-            get { return _donatorService; }
-        public CerereMedicPacientService CerereService
+	    public DonatorService DonatorService
+	    {
+		    get { return _donatorService; }
+	    }
+
+	    public CerereMedicPacientService CerereService
         {
             get { return _cerereService; }
         }
