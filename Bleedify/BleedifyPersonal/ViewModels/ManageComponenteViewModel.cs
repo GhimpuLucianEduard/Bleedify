13417b878850595cd3266c038d35b0b218ae2e63
Kovacs Eduard <edy.kovacs3@gmail.com>
2018-05-29 15:07:55 +0300

    filter compoenente in view-ul de personal

--- Bleedify/BleedifyPersonal/ViewModels/ManageComponenteViewModel.cs
+++ Bleedify/BleedifyPersonal/ViewModels/ManageComponenteViewModel.cs
@@ -1,4 +1,5 @@
-﻿using BleedifyPersonal.Views;
+﻿using BleedifyModels.Enums;
+using BleedifyPersonal.Views;
 using BleedifyServices;
 using DomainViewModels;
 using DomainViewModels.Commands;
@@ -15,6 +16,8 @@ namespace BleedifyPersonal.ViewModels
 {
     public class ManageComponenteViewModel : BaseViewModel
     {
+        private string _selectedStareComponenta;
+        private string _selectedTipComponenta;
         private bool _isDataLoaded;
 
         private ComponentaViewModel _selectedComponenta;
@@ -25,16 +28,110 @@ namespace BleedifyPersonal.ViewModels
         }
 
         public ObservableCollection<ComponentaViewModel> Componente { get; private set; } = new ObservableCollection<ComponentaViewModel>();
+        public ObservableCollection<string> Stari { get; private set; }
+        public ObservableCollection<string> Tipuri { get; private set; }
 
         public ICommand LoadComponenteCommand { get; private set; }
         public ICommand DeleteDonatieCommand { get; private set; }
         public ICommand UpdateCommand { get; private set; }
+        public ICommand FilterComponenteCommand { get; private set; }
+        public ICommand ClearFilterComponenteCommand { get; private set; }
 
         public ManageComponenteViewModel()
         {
+            Stari = new ObservableCollection<string>();
+            Stari.Add("Toate");
+            foreach (var stare in Enum.GetValues(typeof(StareCerere)))
+            {
+                Stari.Add(stare.ToString());
+            }
+            SelectedStareComponenta = Stari[0];
+
+            Tipuri = new ObservableCollection<string>();
+            Tipuri.Add("Toate");
+            foreach (var tip in Enum.GetValues(typeof(TipComponenta)))
+            {
+                Tipuri.Add(tip.ToString());
+            }
+            SelectedTipComponenta = Tipuri[0];
+
             LoadComponenteCommand = new BasicCommand(LoadData);
             DeleteDonatieCommand = new BasicCommand(DeleteComponenta);
             UpdateCommand = new BasicCommand(UpdateComponenta);
+            FilterComponenteCommand = new BasicCommand(FilterComponente);
+            ClearFilterComponenteCommand = new BasicCommand(ClearFilterComponente);
+        }
+
+        private void FilterComponente()
+        {
+            string ParamStare;
+            string ParamTip;
+
+            if (SelectedStareComponenta.CompareTo(Stari[0]) == 0)
+            {
+                ParamStare = null;
+            }
+            else
+            {
+                if (SelectedStareComponenta.Equals("InAsteptare"))
+                {
+                    ParamStare = "In Asteptare";
+                }
+                else
+                {
+                    ParamStare = SelectedStareComponenta;
+                }
+            }
+
+            if (SelectedTipComponenta.CompareTo(Tipuri[0]) == 0)
+            {
+                ParamTip = null;
+            }
+            else
+            {
+                if(SelectedTipComponenta.Equals("GlobuleRosii"))
+                {
+                    ParamTip = "GlobuleRosii";
+                }
+                else
+                {
+                    ParamTip = SelectedTipComponenta;
+                }
+            }
+
+            var comp = AppService.Instance.ComponentaService.Filter(ParamTip, ParamStare);
+
+            Componente.Clear();
+            foreach (var c in comp)
+            {
+                Componente.Add(new ComponentaViewModel(c));
+            }
+        }
+
+        private void ClearFilterComponente()
+        {
+            var comp = AppService.Instance.ComponentaService.GetAll();
+
+            Componente.Clear();
+            foreach (var c in comp)
+            {
+                Componente.Add(new ComponentaViewModel(c));
+            }
+
+            SelectedStareComponenta = Stari[0];
+            SelectedTipComponenta = Tipuri[0];
+        }
+
+        public string SelectedTipComponenta
+        {
+            get { return _selectedTipComponenta; }
+            set { SetValue(ref _selectedTipComponenta, value); }
+        }
+
+        public string SelectedStareComponenta
+        {
+            get { return _selectedStareComponenta; }
+            set { SetValue(ref _selectedStareComponenta, value); }
         }
 
         private void LoadData()
