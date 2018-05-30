6862c6d638c396a23d89a1b2b89684f890207354
Ghimpu Lucian Eduard <ghimpulucianeduard@gmail.com>
2018-05-30 09:32:00 +0300

    Working on donator.

---
+++ Bleedify/BleedifyDonator/ViewModels/MesajeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using BleedifyModels.ModelsEF;
+using BleedifyServices;
+using DomainViewModels;
+
+namespace BleedifyDonator.ViewModels
+{
+	public class MesajeViewModel : BaseViewModel
+	{
+		public ObservableCollection<AnuntDonator> Mesaje;
+
+		public MesajeViewModel()
+		{
+			Mesaje = new ObservableCollection<AnuntDonator>(AppService.Instance.AnuntDonatorService.GetAll());
+		}
+	}
+}
